43ac2256a48304af67d546c46ef184e0f286db67
JOAO VITOR NENES CIZESKI <joao.cizeski@alunos.sc.senac.br>
2026-08-13 21:40:28 -0300

    primeiro commit

--- src/main/java/com/example/tigregym/controllers/UsuarioController.java
+++ src/main/java/com/example/tigregym/controllers/UsuarioController.java
@@ -1,4 +1,26 @@
 package com.example.tigregym.controllers;
 
+import com.example.tigregym.entidades.Usuario;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
+
+    @GetMapping("/listar")
+    public ResponseEntity<?> listarTodos(){
+        List<Usuario> usuarios = List.of(new Usuario(1L,
+                "João",
+                "11698652917",
+                "12345",
+                "joao.cizeski@alunos.sc.senac.br"));
+
+        return ResponseEntity.ok("Sucesso");
+    }
 }
